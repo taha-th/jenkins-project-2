3bb366e4214b51d36201c3e731a76a082979f896
Taha Meer <115108870+taha-th@users.noreply.github.com>
2023-01-06 23:55:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hey I m the king of devops and hamza is my junior");
+  res.send("Hey I m the king of devops and hamza is my junior fuck you");
 });
 
 app.get("/me", (req, res) => {
